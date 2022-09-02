23d2b89220c740a3ba75d2a9375145d1dd55b798
Mohit0234 <78607062+Mohit0234@users.noreply.github.com>
2022-09-02 23:00:59 +0530

    Add files via upload

---
+++ Basics.cpp/Add 2 number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int A;
+    int B;
+    int C;
+    cout<<"Enter the values of A and B:";
+    cin>>A>>B;
+    C=A+B;
+    cout<<"Total sum of number A+B="<<C<<endl;
+    return 0;
+}
