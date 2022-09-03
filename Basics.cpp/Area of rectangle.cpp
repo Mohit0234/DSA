53513821527c5cbeb50c34b6973780a8315d9cad
Mohit0234 <78607062+Mohit0234@users.noreply.github.com>
2022-09-03 21:30:35 +0530

    Add files via upload

---
+++ Basics.cpp/Area of rectangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+//Area of Rectangle
+//A=L*B
+int main()
+{
+    float A,L,B;
+    cout<<"Enter the length:"<<endl;
+    cin>>A;
+    cout<<"Enter the breath:"<<endl;
+    cin>>B;
+    L=A*B;
+    cout<<"Area of Rectangle is="<<L<<endl;
+}
+
