64ce80fa49fa8cbeaceb26a6ceef4d7541d46884
Mohit Puri <78607062+Mohit0234@users.noreply.github.com>
2022-09-08 23:37:24 +0530

    condional

---
+++ Basics.cpp/condition.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a;
+        cout<<" value of a is :" <<endl;
+    cin>>a; 
+    if(a>0) {
+        cout<<" A is Positive" << endl;
+    }
+    else{
+        cout<<" A is negative" << endl;
+    }
+}
